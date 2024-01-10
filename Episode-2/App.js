a5db19c0cd3eacac1e4ae46a93e48937f0be1a6a
Anup Kumar Jana <janaanup7872@gmail.com>
2024-01-10 10:30:16 +0530

    added parcel and react to our project

--- Episode-2/App.js
+++ Episode-2/App.js
@@ -1,21 +1,7 @@
-// const heading = React.createElement("h1", {id:"heading", className:"text-color"}, "Hello fron CDN React"); //tag, object(attributes), content
-// const root = ReactDOM.createRoot(document.getElementById("root")); //to create root we need ReactDOM
+import React from "react"
+import ReactDOM from "react-dom/client";
 
-// root.render(heading);
-
-// console.log(heading);
-
-/*createElement create an object which is {
-    "id": "heading",
-    "className": "text-color",
-    "children": "Hello fron CDN React"
-}*/
-
-/* And .render() takes the object and convert to h1 tag(HTML) into the DOM and add to the root which browser understands*/
-
-//creating nested html elements
-
-const parent = React.createElement("div", { id: "parent" }, [
+const parent = React.createElement("div", { id: "parent", key:1 }, [
   React.createElement("div", { id: "child1", key: 1 }, [
     React.createElement("h1", { id: "heading1", key: 1 }, "this is an h1 tag"),
     React.createElement("h2", { id: "heading2", key: 2 }, "this is an h2 tag"),
@@ -28,7 +14,4 @@ const parent = React.createElement("div", { id: "parent" }, [
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
 
-console.log(parent);
-
-//react can be worked in small portion of our app. like which div(root) will be renderd, only that part will be working in react
-//and thats why React is a library
+// console.log(parent);
