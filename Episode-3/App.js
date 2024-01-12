73a1161ebf1dacc7fd26b01ece57697321775bf9
Anup Kumar Jana <janaanup7872@gmail.com>
2024-01-12 21:36:05 +0530

    added how to crete react element and components into roo.render

--- Episode-3/App.js
+++ Episode-3/App.js
@@ -1,17 +1,42 @@
-import React from "react"
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent", key:1 }, [
-  React.createElement("div", { id: "child1", key: 1 }, [
-    React.createElement("h1", { id: "heading1", key: 1 }, "this is an h1 tag"),
-    React.createElement("h2", { id: "heading2", key: 2 }, "this is an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2", key: 2 }, [
-    React.createElement("h1", { id: "heading1", key: 1 }, "this is an h1 tag"),
-    React.createElement("h2", { id: "heading2", key: 2 }, "this is an h2 tag"),
-  ]),
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// // This is a react element
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "This is an react element🚀"
+);
 
-// console.log(parent);
+// // JSX- and HTML-like or XML-like syntax
+
+const jsxHeading = <h1>This is an JSX heading</h1>;
+// const div = <div className="box"></div>;
+
+// // these are react functional component
+
+const Heading = () => (
+  <div>
+    <h1>Functional component without return</h1>
+  </div>
+);
+
+// // Component Composition
+
+let a = 34;
+
+const Heading1 = () => {
+  return (
+    <div>
+      <Heading />
+
+      <h1>Functional component with return {a}</h1>
+      <span>{heading}</span>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(jsxHeading);
+root.render(<Heading1 />);
